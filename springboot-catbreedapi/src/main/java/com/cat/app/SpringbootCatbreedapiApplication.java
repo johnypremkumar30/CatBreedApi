9f6bdc09c008bff5935b33baaf7b89b2ce124fb8
Jpremkumar <Johny.Premkumar@xpo.com>
2022-04-22 13:48:49 +0530

    initial commit with maven file

---
+++ springboot-catbreedapi/src/main/java/com/cat/app/SpringbootCatbreedapiApplication.java
@@ -0,0 +1,13 @@
+package com.cat.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCatbreedapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCatbreedapiApplication.class, args);
+	}
+
+}
